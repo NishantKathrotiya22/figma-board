714bab4a96ca6470027dd3b06d510cef9415aef9
Nishant Kathrotiya <nishant@techeniac.com>
2025-07-18 14:28:40 +0530

    Base Resourse calander

---
+++ main.js
@@ -0,0 +1,173 @@
+/// <reference path="libs/event-calendar.d.ts" />
+
+const createCalendar = function () {
+  const ec = EventCalendar.create(document.getElementById("ec"), {
+    view: "resourceTimelineDay",
+    editable: false, // disables general editing
+    durationEditable: false, // disables resizing
+    eventStartEditable: false, // disables dragging
+    events: [
+      {
+        resourceId: "1",
+        title: "Local Test Event",
+        start: "2025-07-18 10:00:00",
+        end: "2025-07-18 12:00:00",
+        editable: false,
+        durationEditable: false,
+        eventStartEditable: false,
+      },
+    ],
+    resources: [
+      {
+        id: 1,
+        title: "Resource A",
+        children: [
+          {
+            id: 11,
+            title: "Resource A1",
+          },
+          {
+            id: 12,
+            title: "Resource A2",
+          },
+        ],
+      },
+      {
+        id: 2,
+        title: "Resource C",
+      },
+      {
+        id: 3,
+        title: "Resource D",
+      },
+      {
+        id: 3,
+        title: "Resource D",
+      },
+      {
+        id: 3,
+        title: "Resource D",
+      },
+
+      {
+        id: 3,
+        title: "Resource D",
+      },
+
+      {
+        id: 3,
+        title: "Resource D",
+      },
+      {
+        id: 3,
+        title: "Resource D",
+      },
+
+      {
+        id: 3,
+        title: "Resource D",
+      },
+      {
+        id: 3,
+        title: "Resource D",
+      },
+      {
+        id: 3,
+        title: "Resource D",
+      },
+
+      {
+        id: 3,
+        title: "Resource D",
+      },
+      {
+        id: 3,
+        title: "Resource D",
+      },
+
+      {
+        id: 3,
+        title: "Resource D",
+      },
+      {
+        id: 3,
+        title: "Resource D",
+      },
+
+      {
+        id: 3,
+        title: "Resource D",
+      },
+      {
+        id: 3,
+        title: "Resource D",
+      },
+      {
+        id: 3,
+        title: "Resource D",
+      },
+
+      {
+        id: 3,
+        title: "Resource D",
+      },
+      {
+        id: 3,
+        title: "Resource D",
+      },
+
+      {
+        id: 3,
+        title: "Resource D",
+      },
+      {
+        id: 3,
+        title: "Resource D",
+      },
+
+      {
+        id: 3,
+        title: "Resource D",
+      },
+      {
+        id: 3,
+        title: "Resource D",
+      },
+      {
+        id: 3,
+        title: "Resource D",
+      },
+
+      {
+        id: 3,
+        title: "Resource D",
+      },
+      {
+        id: 3,
+        title: "Resource D",
+      },
+
+      {
+        id: 3,
+        title: "Resource D",
+      },
+      {
+        id: 3,
+        title: "Resource D",
+      },
+
+      {
+        id: 3,
+        title: "Resource D",
+      },
+      {
+        id: 3,
+        title: "Resource D",
+      },
+    ],
+  });
+};
+
+window.addEventListener("DOMContentLoaded", () => {
+  createCalendar();
+});
